023ef137fc0496247c05cfad8309587982de15c0
Sagar Tomar <sagar.tomar@wednesday.is>
2023-11-30 12:52:59 +0530

    Added an auth guard and used it to protect the cab creation route

--- src/cabs/cab.controller.ts
+++ src/cabs/cab.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
 import { CabService } from "./cab.service";
 import { CreateCabDto } from "./createCab.dto";
 import { Cab } from "./cab.entity";
+import { AuthGuard } from "src/auth/auth.guard";
 
 @Controller("/cab")
 export class CabController {
@@ -9,6 +10,7 @@ export class CabController {
         private readonly cabService: CabService
     ) { }
 
+    @UseGuards(AuthGuard)
     @Post()
     async createCab(@Body() createCabDto: CreateCabDto): Promise<Cab> {
         const cab = await this.cabService.create(createCabDto);
--- src/cabs/cab.module.ts
+++ src/cabs/cab.module.ts
@@ -3,9 +3,10 @@ import { DatabaseModule } from "src/database/database.module";
 import { CabController } from "./cab.controller";
 import { cabProviders } from "./cab.provider";
 import { CabService } from "./cab.service";
+import { AuthModule } from "src/auth/auth.module";
 
 @Module({
-    imports: [DatabaseModule],
+    imports: [DatabaseModule, AuthModule],
     controllers: [CabController],
     providers: [
         ...cabProviders,
